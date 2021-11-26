47dd8640e32896d95e1b581e74c89925877b57cb
Szymon <szy.tyrka@gmail.com>
2021-11-26 15:22:25 +0100

    init project

---
+++ README.md
@@ -0,0 +1,12 @@
+# Social Network - REST API
+
+Server for the social network application.
+
+## Technologies
+- Java 8
+- Spring Boot
+- Spring Data JPA
+- Spring Security
+- JWT
+- JUnit
+
---
+++ src/test/java/com/server/springboot/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
